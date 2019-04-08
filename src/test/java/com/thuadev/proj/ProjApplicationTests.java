e4e3864fe50c2b1223f297422a2ace70a0df61aa
thua <overflow_hua@163.com>
2019-04-08 18:12:18 +0800

    单元测试，jpa条件查询,增加actuator

--- src/test/java/com/thuadev/proj/ProjApplicationTests.java
+++ src/test/java/com/thuadev/proj/ProjApplicationTests.java
@@ -1,16 +1,32 @@
 package com.thuadev.proj;
 
+import com.thuadev.proj.pojo.UserEntity;
+import com.thuadev.proj.service.UserService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Date;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ProjApplicationTests {
-
+    @Autowired
+    private UserService userService;
     @Test
     public void contextLoads() {
     }
-
+    @Test
+    public void addUser(){
+        UserEntity userEntity = new UserEntity();
+        Date date = new Date();
+        userEntity.setCreated(date);
+        userEntity.setUpdated(date);
+        userEntity.setUserName("小D");
+        userService.addUser(userEntity);
+        long id = userService.findMaxUserId();
+        System.out.println(id);
+    }
 }
